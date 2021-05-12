7e986cd027edb8557110cc4ff7bf204148948f29
ricardo <ricardo.ortiz@trade.ec>
2021-05-12 15:56:50 -0500

    Updated docker-compose version to 1.29.1

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,5 +1,5 @@
-https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-20-04
+#https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-20-04
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
